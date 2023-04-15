d75ab4dc8c27178ca7de42086ffbd293bc3e9be1
DavideAtzo <atzo9477@gmail.com>
2023-04-15 11:53:27 +0200

    aggiunta costrutto

--- index.php
+++ index.php
@@ -4,6 +4,15 @@ class Movie
     public $title;
     public $type;
     public $year;
-}
+    function __construct($title, $type, $year)
+    {
+        $this->title = $title;
+        $this->type = $type;
+        $this->year = $year;
+    }
 
-?>
+    function get_title()
+    {
+        return $this->title;
+    }
+}
