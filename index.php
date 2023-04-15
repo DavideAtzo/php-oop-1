6a31fbd24476f6c6841ecf8f7603eb2a9f0e0d16
DavideAtzo <atzo9477@gmail.com>
2023-04-15 12:03:17 +0200

    stampa dei film

--- index.php
+++ index.php
@@ -16,3 +16,18 @@ class Movie
         return $this->title;
     }
 }
+$movie1 = new Movie("Il Signore degli Anelli", "Fantasy", "2001");
+$movie2 = new Movie("The Departed - Il bene e il male", "Thriller", "2006");
+$movie3 = new Movie("Star Wars - Una nuova speranza", "Fantasy", "1977");
+
+echo "Title: " . $movie1->get_title() . "<br>";
+echo "Director: " . $movie1->type . "<br>";
+echo "Year: " . $movie1->year . "<br><hr>";
+
+echo "Title: " . $movie2->get_title() . "<br>";
+echo "Director: " . $movie2->type . "<br>";
+echo "Year: " . $movie2->year . "<br><hr>";
+
+echo "Title: " . $movie3->get_title() . "<br>";
+echo "Director: " . $movie3->type . "<br>";
+echo "Year: " . $movie3->year . "<br>";
